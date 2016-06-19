c55a4672869e819deac8161b0b04b7951b95e635
Alexey Peskov <aspeskov@gmail.com>
2016-06-19 19:50:26 +0200

    Assignment 1 impletemted

---
+++ src/com/sandlex/algo1/InversionsCounter.java
@@ -0,0 +1,72 @@
+package com.sandlex.algo1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * This file contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order,
+ * with no integer repeated.
+ *
+ * Your task is to compute the number of inversions in the file given, where the ith row of the file
+ * indicates the ith entry of an array.
+ *
+ * Because of the large size of this array, you should implement the fast divide-and-conquer algorithm
+ * covered in the video lectures.
+ *
+ *
+ * author: Alexey Peskov
+ */
+public class InversionsCounter {
+
+    private static Long inversions;
+
+    public static void main(String[] args) throws IOException {
+        Stream<String> stream = Files.lines(Paths.get("/Users/sandlex/dev/algo1/resources/com/sandlex/algo1/_bcb5c6658381416d19b01bfc1d3993b5_IntegerArray.txt"));
+        List<Integer> integers = new ArrayList<>();
+        stream.mapToInt(Integer::parseInt).forEach(integers::add);
+        inversions = 0l;
+        recursiveSort(integers);
+        System.out.println(inversions);
+    }
+
+    private static List<Integer> recursiveSort(List<Integer> ints) {
+        if (ints.size() > 1) {
+            List<Integer> a = recursiveSort(ints.subList(0, ints.size() / 2));
+            List<Integer> b = recursiveSort(ints.subList(ints.size() / 2, ints.size()));
+            return merge(a, b);
+        }
+
+        return ints;
+    }
+
+    private static List<Integer> merge(List<Integer> a, List<Integer> b) {
+        List<Integer> res = new ArrayList<Integer>();
+        int k = a.size() + b.size();
+        int i = 0, j = 0;
+
+        for (int c = 0; c < k; c++) {
+            if (i >= a.size() || j >= b.size()) {
+                if (i >= a.size()) {
+                    res.add(b.get(j));
+                    j++;
+                } else {
+                    res.add(a.get(i));
+                    i++;
+                }
+            } else if (a.get(i) < b.get(j)) {
+                res.add(a.get(i));
+                i++;
+            } else {
+                res.add(b.get(j));
+                j++;
+                inversions += (a.size() - i);
+            }
+        }
+
+        return res;
+    }
+}
